434b39c6d4a2f2dba8c511887e44d088ba840b6f
haydenm58 <hayden.mick58@gmail.com>
2024-01-02 00:58:23 -0700

    Section 18 Final

--- Section 18/01-starting-project/01-starting-project/src/store/CartContext.jsx
+++ Section 18/01-starting-project/01-starting-project/src/store/CartContext.jsx
@@ -4,6 +4,7 @@ const CartContext = createContext({
   items: [],
   addItem: (item) => {},
   removeItem: (id) => {},
+  clearCart: () => {},
 });
 
 function cartReducer(state, action) {
@@ -35,7 +36,7 @@ function cartReducer(state, action) {
     const existingCartItem = state.items[existingCartItemIndex];
 
     const updatedItems = [...state.items];
-    
+
     if (existingCartItem.quantity === 1) {
       updatedItems.splice(existingCartItemIndex, 1);
     } else {
@@ -49,29 +50,40 @@ function cartReducer(state, action) {
     return { ...state, items: updatedItems };
   }
 
+  if (action.type === "CLEAR_CART") {
+    return { ...state, items: [] };
+  }
+
   return state;
 }
 
 export function CartContextProvider({ children }) {
-  const [ cart, dispatchCartAction ] = useReducer(cartReducer, { items: [] });
-  
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
+
   function addItem(item) {
-      dispatchCartAction({ type: 'ADD_ITEM', item })
-    }
-    
-    function removeItem(id) {
-        dispatchCartAction({ type: 'REMOVE_ITEM', id })
-    }
-    
-    const cartContext = {
-      items: cart.items,
-      addItem,
-      removeItem
-    };
+    dispatchCartAction({ type: "ADD_ITEM", item });
+  }
 
-    console.log(cartContext);
+  function removeItem(id) {
+    dispatchCartAction({ type: "REMOVE_ITEM", id });
+  }
 
-    return <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>;
+  function clearCart() {
+    dispatchCartAction({ type: "CLEAR_CART" });
+  }
+
+  const cartContext = {
+    items: cart.items,
+    addItem,
+    removeItem,
+    clearCart
+  };
+
+  console.log(cartContext);
+
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
 }
 
 export default CartContext;
